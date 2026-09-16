e17b2294daddf542ca166847789c2c1f9cddf037
Parth Panchal <parthpanchal1352003@gmail.com>
2026-09-16 21:09:27 +0530

    Fixed Playstore icon

--- src/components/PlayStoreIcon.tsx
+++ src/components/PlayStoreIcon.tsx
@@ -6,8 +6,13 @@ interface PlayStoreIconProps {
 
 export const PlayStoreIcon: React.FC<PlayStoreIconProps> = ({ className = 'w-6 h-6' }) => {
   return (
-    <svg viewBox="0 0 24 24" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path d="M3.609 1.814L13.792 12 3.61 22.186a.996.996 0 0 1-.61-.92V2.734a1 1 0 0 1 .609-.92zm10.89 10.893l2.302 2.302-10.937 6.333 8.635-8.635zm3.199-3.198l2.807 1.626a1 1 0 0 1 0 1.73l-2.808 1.626L15.408 12l2.29-2.491zM5.864 2.658L16.802 8.99l-2.302 2.302-8.635-8.634z" />
+    <svg viewBox="0 0 100 100" className={className} xmlns="http://www.w3.org/2000/svg">
+      {/* Play-triangle fan: green (top), yellow (tip), red (bottom) */}
+      <path d="M22,12 L22,36 L90,50 Z" fill="#00E676" />
+      <path d="M22,36 L22,64 L90,50 Z" fill="#FFCE00" />
+      <path d="M22,64 L22,88 L90,50 Z" fill="#FF3A44" />
+      {/* Blue diagonal stroke overlaid on top, like the real logo */}
+      <path d="M22,12 L58,50 L22,88 Z" fill="#32BBFF" />
     </svg>
   );
 };
